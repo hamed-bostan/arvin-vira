6baf04ede2e12a44721a5abb03cb510e07b4e694
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-03-07 16:52:54 +0330

    fix(landing): improve styles in itemsList

--- src/components/common/ItemsList.js
+++ src/components/common/ItemsList.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function ItemsList({ mainText, highlightedText, description }) {
   return (
     <div className="mb-12 text-[#FAFAFA]">
-      <span className="mx-auto block text-sm leading-[22px] px-6 py-2 bg-[#393C3E] rounded-lg mb-1 border border-[#4a4a4a] w-fit">
+      <span className="mx-auto block text-sm leading-[22px] px-6 py-2 bg-[#393C3E] rounded-lg mb-1 border border-[#4a4a4a] w-fit cursor-pointer">
         راه‌کار ‌ها
       </span>
       <h2 className="text-3xl leading-[57px] mb-6 text-center">
@@ -15,7 +15,7 @@ export default function ItemsList({ mainText, highlightedText, description }) {
       <p className="leading-[32px] mb-6 text-center max-w-[796px]">
         {description}
       </p>
-      <div className="bg-[#FB6514] flex gap-x-2 rounded-md px-3 py-2 items-center w-fit mx-auto">
+      <div className="bg-[#FB6514] flex gap-x-2 rounded-md px-3 py-2 items-center w-fit mx-auto cursor-pointer">
         <span>مطالعه بیشتر</span>
         <Image
           src={"/icon/arrowLeftWhite.svg"}
