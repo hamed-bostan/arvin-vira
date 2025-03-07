d346c902e908ce0c657a2a7df28f475209388b0c
Hamed Bostan <Hamed.Bostan2@gmail.com>
2025-03-07 15:47:52 +0330

    install mui

---
+++ theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1C64F2",
+    },
+    secondary: {
+      main: "#AECE3B",
+    },
+  },
+});
+
+export default theme;
